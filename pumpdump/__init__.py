f820b40001d5bbee6f88e8b5377a965badc5a6c0
len <lenwhite@users.noreply.github.com>
2021-07-26 02:27:31 +0000

    Make importing more convenient

--- pumpdump/__init__.py
+++ pumpdump/__init__.py
@@ -1,4 +1,6 @@
 # flake8: noqa
 from pumpdump._config import PlatformConfig, SymbolConfig
+from pumpdump.actor.random_walk import RandomWalk
+from pumpdump.platform import Platform
 
 default_config = PlatformConfig()
